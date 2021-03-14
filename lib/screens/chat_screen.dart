23b27a0a278d3a34605ce6ff76fd335b4eba1c77
Warley Soares <wss@cin.ufpe.br>
2021-03-13 21:23:02 -0300

    widget streamBuilder used and messages printed on chatScreen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Center(child: Text("Chat"))),
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, i) => Container(
-                padding: EdgeInsets.all(10),
-                child: Text("Funfou!"),
-              )),
-    );
+        appBar: AppBar(title: Center(child: Text("Chat"))),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              final documents = snapshot.data.docs;
+              return ListView.builder(
+                  itemCount: documents.length,
+                  itemBuilder: (context, i) => Container(
+                        padding: EdgeInsets.all(10),
+                        child: Text(documents[i]['text']),
+                      ));
+            }
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ));
   }
 }
